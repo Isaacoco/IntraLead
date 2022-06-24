cf513f46d67f8c549e2521a1e05dc2df8d50f470
Zack <57871488+Isaacoco@users.noreply.github.com>
2022-06-24 20:26:26 +0200

    Version fonctionnelle 1

--- src/Entity/Lead.php
+++ src/Entity/Lead.php
@@ -19,6 +19,24 @@ class Lead
     #[ORM\Column(type: 'string', length: 20)]
     private $Prenom;
 
+    #[ORM\Column(type: 'integer')]
+    private $id_statut;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $adresse;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $cp;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $ville;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $tel;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $email;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -44,6 +62,78 @@ class Lead
     public function setPrenom(string $Prenom): self
     {
         $this->Prenom = $Prenom;
+
+        return $this;
+    }
+
+    public function getIdStatut(): ?int
+    {
+        return $this->id_statut;
+    }
+
+    public function setIdStatut(int $id_statut): self
+    {
+        $this->id_statut = $id_statut;
+
+        return $this;
+    }
+
+    public function getAdresse(): ?string
+    {
+        return $this->adresse;
+    }
+
+    public function setAdresse(string $adresse): self
+    {
+        $this->adresse = $adresse;
+
+        return $this;
+    }
+
+    public function getCp(): ?string
+    {
+        return $this->cp;
+    }
+
+    public function setCp(string $cp): self
+    {
+        $this->cp = $cp;
+
+        return $this;
+    }
+
+    public function getVille(): ?string
+    {
+        return $this->ville;
+    }
+
+    public function setVille(string $ville): self
+    {
+        $this->ville = $ville;
+
+        return $this;
+    }
+
+    public function getTel(): ?string
+    {
+        return $this->tel;
+    }
+
+    public function setTel(string $tel): self
+    {
+        $this->tel = $tel;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
 
         return $this;
     }
